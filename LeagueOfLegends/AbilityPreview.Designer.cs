b64262d5d8e842fb2df56e24e0d35df4d27d0327
Conor Shand <Conor.Shand@hotmail.com>
2017-05-28 17:01:37 +0100

    Basic Form Layout + Initial "Retrieving Data" functionality

--- LeagueOfLegends/AbilityPreview.Designer.cs
+++ LeagueOfLegends/AbilityPreview.Designer.cs
@@ -28,20 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.leagueOfLegendsDataSet = new LeagueOfLegends.LeagueOfLegendsDataSet();
+            this.lblChampions = new System.Windows.Forms.Label();
+            this.lstChampions = new System.Windows.Forms.ListBox();
+            this.lstAbilities = new System.Windows.Forms.ListBox();
+            this.lblAbilities = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.leagueOfLegendsDataSet)).BeginInit();
             this.SuspendLayout();
+            // 
+            // leagueOfLegendsDataSet
+            // 
+            this.leagueOfLegendsDataSet.DataSetName = "LeagueOfLegendsDataSet";
+            this.leagueOfLegendsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // lblChampions
+            // 
+            this.lblChampions.AutoSize = true;
+            this.lblChampions.Location = new System.Drawing.Point(64, 71);
+            this.lblChampions.Name = "lblChampions";
+            this.lblChampions.Size = new System.Drawing.Size(59, 13);
+            this.lblChampions.TabIndex = 0;
+            this.lblChampions.Text = "Champions";
+            // 
+            // lstChampions
+            // 
+            this.lstChampions.FormattingEnabled = true;
+            this.lstChampions.Location = new System.Drawing.Point(31, 98);
+            this.lstChampions.Name = "lstChampions";
+            this.lstChampions.Size = new System.Drawing.Size(134, 199);
+            this.lstChampions.TabIndex = 1;
+            this.lstChampions.SelectedIndexChanged += new System.EventHandler(this.lstChampions_SelectedIndexChanged);
+            // 
+            // lstAbilities
+            // 
+            this.lstAbilities.FormattingEnabled = true;
+            this.lstAbilities.Location = new System.Drawing.Point(212, 98);
+            this.lstAbilities.Name = "lstAbilities";
+            this.lstAbilities.Size = new System.Drawing.Size(797, 199);
+            this.lstAbilities.TabIndex = 3;
+            this.lstAbilities.Format += new System.Windows.Forms.ListControlConvertEventHandler(this.lstAbilities_Format);
+            // 
+            // lblAbilities
+            // 
+            this.lblAbilities.AutoSize = true;
+            this.lblAbilities.Location = new System.Drawing.Point(260, 71);
+            this.lblAbilities.Name = "lblAbilities";
+            this.lblAbilities.Size = new System.Drawing.Size(42, 13);
+            this.lblAbilities.TabIndex = 2;
+            this.lblAbilities.Text = "Abilities";
             // 
             // AbilityPreview
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1216, 536);
+            this.Controls.Add(this.lstAbilities);
+            this.Controls.Add(this.lblAbilities);
+            this.Controls.Add(this.lstChampions);
+            this.Controls.Add(this.lblChampions);
             this.Name = "AbilityPreview";
             this.Text = "Ability Preview";
+            this.Load += new System.EventHandler(this.AbilityPreview_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.leagueOfLegendsDataSet)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private LeagueOfLegendsDataSet leagueOfLegendsDataSet;
+        private System.Windows.Forms.Label lblChampions;
+        private System.Windows.Forms.ListBox lstChampions;
+        private System.Windows.Forms.ListBox lstAbilities;
+        private System.Windows.Forms.Label lblAbilities;
     }
 }
 
--- LeagueOfLegends/AbilityPreview.cs
+++ LeagueOfLegends/AbilityPreview.cs
@@ -7,14 +7,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace LeagueOfLegends
 {
     public partial class AbilityPreview : Form
     {
+        SqlConnection connection;
+        string connectionString;
+
         public AbilityPreview()
         {
             InitializeComponent();
+
+            //Initialize Connection String when Form Object is created.
+            connectionString = ConfigurationManager.ConnectionStrings["LeagueOfLegends.Properties.Settings.LeagueOfLegendsConnectionString"].ConnectionString;
+
+        }
+        private void AbilityPreview_Load(object sender, EventArgs e)
+        {
+            PopulateChampions();
+            PopulateAbilities();
+
+        }
+
+        private void PopulateChampions()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Champion", connection))
+            {
+                DataTable championTable = new DataTable();
+                adapter.Fill(championTable);
+
+                lstChampions.DisplayMember = "Name";
+                lstChampions.ValueMember = "Id";
+                lstChampions.DataSource = championTable;
+            }
+
+
+
+            
+        }
+
+        private void PopulateAbilities()
+        {
+            string sqlQuery = "SELECT a.name, a.description FROM Abilities a " + "INNER JOIN ChampionAbilities b ON a.Id = b.AbilityId " + "WHERE b.ChampionID = @ChampionId";
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(sqlQuery, connection)) 
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@ChampionId", lstChampions.SelectedValue);
+
+                DataTable abilityTable = new DataTable();
+                adapter.Fill(abilityTable);
+
+                lstAbilities.MultiColumn = true;
+                lstAbilities.DisplayMember = "Name";
+                lstAbilities.ValueMember = "Id";
+                lstAbilities.DataSource = abilityTable;
+            }
+
+
+
+
+        }
+
+        private void lstChampions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateAbilities();
+        }
+
+        private void lstAbilities_Format(object sender, ListControlConvertEventArgs e)
+        {
         }
     }
 }
